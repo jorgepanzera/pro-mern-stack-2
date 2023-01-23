f58f9cb76a2465178dc77e17bfa9638185534a67
Jorge <panzera.jorge@gmail.com>
2023-01-23 13:14:46 -0300

    Inicializar repo

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// Importar express http server
+const express = require("express");
+
+// crear aplicacion express
+const app = express();
+
+// para usar el absolute path de una aplicacion
+const path = require("path");
+
+// funcion de express static para acceder al contenido de una carpeta
+//console.log(path.join(__dirname, 'public'));
+const fileServerMiddlewareconst = express.static(path.join(__dirname,'..','public')); // _dirname/.. me deja en la carpeta raiz
+
+// usar dicha carpeta
+app.use('/', fileServerMiddlewareconst);
+
+// start server en puerto 3000
+app.listen(3000, function () {
+    console.log('App started on port 3000');
+    });
