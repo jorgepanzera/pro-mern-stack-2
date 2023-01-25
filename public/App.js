b6ea596bed6a709d69056184de5b4105536e291a
Jorge <panzera.jorge@gmail.com>
2023-01-25 15:02:42 -0300

    debug semi funcionando

--- public/App.js
+++ public/App.js
@@ -4,7 +4,7 @@ var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
 var helloContinents = Array.from(continents, function (c) {
   return "Hello ".concat(c, " !!");
 });
-var message = helloContinents.join(' # ');
+var message = helloContinents.join(' -- ');
 var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
 }, /*#__PURE__*/React.createElement("h1", null, message));
