d1c5a24d85f570cc09c5cb8f3531b32a0a236be5
Jorge <panzera.jorge@gmail.com>
2023-01-25 12:41:03 -0300

    Intentando debug desde vsCode

--- public/App.js
+++ public/App.js
@@ -4,8 +4,9 @@ var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
 var helloContinents = Array.from(continents, function (c) {
   return "Hello ".concat(c, " !!");
 });
-var message = helloContinents.join(' // ');
+var message = helloContinents.join(' # ');
 var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
 }, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
+//# sourceMappingURL=App.js.map
