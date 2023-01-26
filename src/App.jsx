f9737e17fe5cae083bd45f99f4ca1a5c85cdcfef
Jorge <panzera.jorge@gmail.com>
2023-01-26 13:17:48 -0300

    chapter 3 comienzo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-
-const continents = ['Africa','America','Asia','Australia','Europe'];
-
-const helloContinents = Array.from(continents, c => `Hello ${c} !!`);
-const message = helloContinents.join(' *** ');
-
-const element = (
-    <div title="Outer div" >
-      <h1>{message}</h1>
-    </div>
+class IssueFilter extends React.Component {
+  render() {
+    return (
+    <div>This is a placeholder for the issue filter.</div>
   );
+  }
+ }
 
-  ReactDOM.render(element, document.getElementById('contents'));
+ class IssueTable extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a table of issues.</div>
+    );
+  }
+ }
+ 
+ class IssueAdd extends React.Component {
+  render() {
+  return (
+  <div>This is a placeholder for a form to add an issue.</div>
+  );
+  }
+ }
+
+ // clase que usa las otras 3 en un Fragment
+ class IssueList extends React.Component {
+  render() {
+  return (
+  <React.Fragment>
+    <h1>Issue Tracker</h1>
+    <IssueFilter />
+    <hr />
+    <IssueTable />
+    <hr />
+    <IssueAdd />
+  </React.Fragment>
+  );
+  }
+ }
+
+ // Crear issue list
+const element = <IssueList />;
+
+ReactDOM.render(element, document.getElementById('contents'));
