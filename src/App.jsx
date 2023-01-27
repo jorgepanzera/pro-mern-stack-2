78830983c42ed2253acbff4e9d34c646280b09c6
Jorge <panzera.jorge@gmail.com>
2023-01-27 12:48:04 -0300

    chapter 3 parameters

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,45 @@ class IssueFilter extends React.Component {
   }
  }
 
+ class IssueRow extends React.Component {
+  render() {
+
+    const style = this.props.rowStyle;
+
+    return (
+      <tr>
+        <td style={style}> {this.props.issue_id} </td>
+        <td style={style}> {this.props.issue_title} </td>
+      </tr>
+    );
+  }
+ }
+
  class IssueTable extends React.Component {
   render() {
-    return (
-      <div>This is a placeholder for a table of issues.</div>
-    );
+
+    const myStyle = this.props.tableStyle;
+
+    const rowStyle = {
+      border: "1px solid orange",
+      padding: 4
+    }
+
+
+  return (
+    <table style={myStyle}>
+      <thead>
+        <tr>
+          <th style={rowStyle}>ID</th>
+          <th style={rowStyle}>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <IssueRow rowStyle={rowStyle} issue_id={1} issue_title={"Error in console when clicking Add"} />
+        <IssueRow rowStyle={rowStyle} issue_id={2} issue_title={"Missing bottom border on right panel"} />
+      </tbody>
+    </table>
+  );
   }
  }
  
@@ -23,15 +57,22 @@ class IssueFilter extends React.Component {
  }
 
  // clase que usa las otras 3 en un Fragment
- class IssueList extends React.Component {
+ class IssueList extends React.Component { 
   render() {
+
+     const tableStyle = {
+      marginLeft: "auto",
+      marginRight: "auto",
+      borderCollapse: "collapse"
+    }
+
   return (
   <React.Fragment>
     <h1>Issue Tracker</h1>
     <IssueFilter />
     <hr />
-    <IssueTable />
-    <hr />
+    <IssueTable tableStyle={tableStyle} />
+      <hr />
     <IssueAdd />
   </React.Fragment>
   );
@@ -39,6 +80,6 @@ class IssueFilter extends React.Component {
  }
 
  // Crear issue list
-const element = <IssueList />;
+const element = <IssueList  / >;
 
 ReactDOM.render(element, document.getElementById('contents'));
